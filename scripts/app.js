ce68e7e178765dfad6d6b1fa5661b04d0df4e735
derbakias <dimitrios.danousis@gmail.com>
2020-01-10 09:58:10 +0200

    turn safesearch=true

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,7 @@ form.addEventListener('submit' , (e) => {
   e.preventDefault();
   spinner.style.display = 'flex';
   const weatherSource = `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=cb4b316ea7ff3ec1b3b96455c376cb8a`;
-  const photoSource = `https://pixabay.com/api/?key=14392760-281ce1fc01330fca172fd23a0&q=${search.value}`
+  const photoSource = `https://pixabay.com/api/?key=14392760-281ce1fc01330fca172fd23a0&q=${search.value}&safesearch="true"`;
   getData(weatherSource, data => {
     getData(photoSource, photo => {
       updateUI(data, photo);
